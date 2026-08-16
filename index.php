cd74604cfeffddefbecd47a069db8d369d7f7931
Joy <joyrivero64@gmail.com>
2026-08-16 14:41:58 -0300

    Crear pantalla inicial de Joy CRM

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombreProyecto ="Joy CRM";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $nombreProyecto;?></title>
+</head>
+<body>
+    <h1><?php echo $nombreProyecto; ?></h1>
+    <p>Central de consultas de Joy Digital</p>
+</body>
+</html>
