11ef90a80d459ffbbadc66be7e3c40a3ee49393a
Joy <joyrivero64@gmail.com>
2026-08-17 11:38:50 -0300

    Agregar estilos de JOY CRM

--- admin/index.php
+++ admin/index.php
@@ -18,6 +18,7 @@ $inquiries = $response["data"] ?? [];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Panel de consultas | Joy CRM</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $nombreProyecto = "Joy CRM";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $nombreProyecto; ?></title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
